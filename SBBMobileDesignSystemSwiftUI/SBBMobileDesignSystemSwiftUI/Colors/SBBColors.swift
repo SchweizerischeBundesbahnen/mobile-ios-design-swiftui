6437a4cc2dbb366e440f47783f226532961de7e1
u214375 <georgios.antoniadis@sbb.ch>
2020-01-23 14:12:21 +0100

    feature: initial commit

---
+++ SBBMobileDesignSystemSwiftUI/SBBMobileDesignSystemSwiftUI/Colors/SBBColors.swift
@@ -0,0 +1,17 @@
+//
+//  SBBColors.swift
+//  SBBMobileDesignSystemSwiftUI
+//
+//  Created by Georgios Antoniadis on 23.01.20.
+//  Copyright © 2020 SBB. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public struct SBBColors {
+    
+    public init() {}
+    
+    public static var sbb_blue = Color(red: 0, green: 1, blue: 0)
+}
---
+++ SBBMobileDesignSystemSwiftUI/SBBMobileDesignSystemSwiftUIDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SBBMobileDesignSystemSwiftUIDemo
+//
+//  Created by Georgios Antoniadis on 23.01.20.
+//  Copyright © 2020 SBB. All rights reserved.
+//
+
+import SwiftUI
+import SBBMobileDesignSystemSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!").foregroundColor(SBBColors.sbb_blue)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
