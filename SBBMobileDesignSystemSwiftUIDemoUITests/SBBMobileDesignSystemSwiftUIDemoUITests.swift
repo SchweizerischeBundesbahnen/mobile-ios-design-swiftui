c15285f936936906c6ddf14347c8f10aef2fd3e9
nicolasbrunner <nicolas.brunner@sbb.ch>
2020-01-24 13:58:55 +0100

    Removed launchPerformance test.

--- SBBMobileDesignSystemSwiftUIDemoUITests/SBBMobileDesignSystemSwiftUIDemoUITests.swift
+++ SBBMobileDesignSystemSwiftUIDemoUITests/SBBMobileDesignSystemSwiftUIDemoUITests.swift
@@ -32,12 +32,12 @@ class SBBMobileDesignSystemSwiftUIDemoUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testLaunchPerformance() {
+    /*func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }
         }
-    }
+    }*/
 }
