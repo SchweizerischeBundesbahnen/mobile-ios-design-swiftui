8bfcdf1e9fb6effaf94b6e183ad35c5a797db427
nicolasbrunner <nicolas.brunner@sbb.ch>
2020-01-24 08:11:35 +0100

    Removed failing UItests (2).

--- SBBMobileDesignSystemSwiftUIDemoUITests/SBBMobileDesignSystemSwiftUIDemoUITests.swift
+++ SBBMobileDesignSystemSwiftUIDemoUITests/SBBMobileDesignSystemSwiftUIDemoUITests.swift
@@ -22,13 +22,4 @@ class SBBMobileDesignSystemSwiftUIDemoUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 }
