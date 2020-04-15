418d4a3fe54022821518d0cb37c1af67d82cfa91
Vessaz François (IT-SWE-CC2-MOB) <francois.vessaz@sbb.ch>
2020-04-15 11:07:23 +0200

    Add localization for form group

--- SBBMobileDesignSystemSwiftUI/Views/SBBFormGroup.swift
+++ SBBMobileDesignSystemSwiftUI/Views/SBBFormGroup.swift
@@ -6,73 +6,113 @@ import SwiftUI
 
 public struct SBBFormGroup: View {
     
-    private let title: String
+    private let title: LocalizedStringKey?
     private let rows: [AnyView]
     
-    public init<C0: View>(title: String = "", @ViewBuilder content: () -> C0) {
-        self.title = title
+    public init<C0: View>(title: String? = nil, @ViewBuilder content: () -> C0) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let row = AnyView(content())
         rows = [row]
     }
     
-    public init<C0: View, C1: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1)>) {
-        self.title = title
+    public init<C0: View, C1: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1)]
     }
     
-    public init<C0: View, C1: View, C2: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4), AnyView(tuple.5)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4), AnyView(tuple.5), AnyView(tuple.6)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4), AnyView(tuple.5), AnyView(tuple.6), AnyView(tuple.7)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4), AnyView(tuple.5), AnyView(tuple.6), AnyView(tuple.7), AnyView(tuple.8)]
     }
     
-    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View, C9: View>(title: String = "", @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)>) {
-        self.title = title
+    public init<C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View, C9: View>(title: String? = nil, @ViewBuilder content: () -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)>) {
+        if let title = title {
+            self.title = LocalizedStringKey(title)
+        } else {
+            self.title = nil
+        }
         let tuple = content().value
         rows = [AnyView(tuple.0), AnyView(tuple.1), AnyView(tuple.2), AnyView(tuple.3), AnyView(tuple.4), AnyView(tuple.5), AnyView(tuple.6), AnyView(tuple.7), AnyView(tuple.8), AnyView(tuple.9)]
     }
     
     public var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            if !title.isEmpty {
-                Text(title)
+            if title != nil {
+                Text(title!)
                     .sbbFont(.body)
                     .padding(.top, 24)
                     .padding([.bottom, .leading, .trailing], 16)
