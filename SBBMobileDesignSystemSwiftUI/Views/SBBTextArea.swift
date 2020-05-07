3b9fc9973c09086a4bc6f82b93422d7b3a6899e7
Vessaz François (IT-SWE-CC2-MOB) <francois.vessaz@sbb.ch>
2020-05-07 16:27:31 +0200

    Editing line bellow SBBTextArea
    Makes all SBBTextArea inner components (relative to UIKit bridging) not public anymore

--- SBBMobileDesignSystemSwiftUI/Views/SBBTextArea.swift
+++ SBBMobileDesignSystemSwiftUI/Views/SBBTextArea.swift
@@ -4,99 +4,128 @@
 
 import SwiftUI
 
-public struct SBBTextArea: UIViewRepresentable {
+public struct SBBTextArea: View {
     
     @Binding private var text: String
+    @State private var isEditing = false
     private var label: String?
-    
-    let descriptionLabel = UILabel()
-    let placeholderLabel = UILabel()
     
     public init(text: Binding<String>, label: String? = nil) {
         self._text = text
         self.label = label
     }
     
-    public func makeUIView(context: Context) -> UITextView {
-        let view = UITextView()
-        view.delegate = context.coordinator
-                        
-        let scaledFontSize = UIFontMetrics.default.scaledValue(for: 16)
-        view.font = UIFont(name: "SBBWeb-Light", size: scaledFontSize)
-        view.textContainerInset = UIEdgeInsets(top: 17, left: 11, bottom: 16, right: 11)
-        view.layer.cornerRadius = 16
-        view.backgroundColor = UIColor(named: "viewBackground", in: Helper.bundle, compatibleWith: nil)
-        let placeholderScaledFontSize = UIFontMetrics.default.scaledValue(for: 10)
-        view.textContainerInset = UIEdgeInsets(top: placeholderScaledFontSize + 7, left: 11, bottom: 16, right: 11)
-        
-        if let placeholder = label {
-            descriptionLabel.text = NSLocalizedString(placeholder, comment: "")
-            view.accessibilityLabel = placeholder
-            view.accessibilityValue = text
-        }
-        let descriptionScaledFontSize = UIFontMetrics.default.scaledValue(for: 10)
-        descriptionLabel.font = UIFont(name: "SBBWeb-Light", size: descriptionScaledFontSize)
-        descriptionLabel.textColor = UIColor(named: "placeholder", in: Helper.bundle, compatibleWith: nil)
-        view.addSubview(descriptionLabel)
-        
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        let descriptionTopConstraint = descriptionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 5)
-        let descriptionLeadingConstraint = descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 17)
-        let descriptionTrailingConstraint = descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        view.addConstraints([descriptionTopConstraint, descriptionLeadingConstraint, descriptionTrailingConstraint])
-        
-        if let placeholder = label {
-            placeholderLabel.text = NSLocalizedString(placeholder, comment: "")
-        }
-        placeholderLabel.font = UIFont(name: "SBBWeb-Light", size: scaledFontSize)
-        placeholderLabel.textColor = UIColor(named: "placeholder", in: Helper.bundle, compatibleWith: nil)
-        view.addSubview(placeholderLabel)
-        
-        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
-        let topConstraint = placeholderLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 17.5)
-        let leadingConstraint = placeholderLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
-        let trailingConstraint = placeholderLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        view.addConstraints([topConstraint, leadingConstraint, trailingConstraint])
-        
-        let hasContent = !text.isEmpty
-        descriptionLabel.isHidden = !hasContent
-        placeholderLabel.isHidden = hasContent
-        
-        return view
-    }
-
-    public func updateUIView(_ uiView: UITextView, context: Context) {
-        uiView.text = text
-        context.coordinator.updatePlaceholder(for: uiView)
-        
-        if context.environment.isEnabled {
-            uiView.textColor = UIColor(named: "textBlack", in: Helper.bundle, compatibleWith: nil)
-        } else {
-            uiView.textColor = UIColor(named: "textMetal", in: Helper.bundle, compatibleWith: nil)
-        }
+    public var body: some View {
+        SBBTextAreaImpl(text: $text, isEditing: $isEditing, label: label)
+            .background((isEditing ? SBBColor.textfieldLineActive : SBBColor.textfieldLineInactive).frame(height: 1), alignment: .bottom)
     }
     
-    public func makeCoordinator() -> TextAreaCoordinator {
-        return TextAreaCoordinator(self)
-    }
-    
-    public class TextAreaCoordinator: NSObject, UITextViewDelegate {
+    private struct SBBTextAreaImpl: UIViewRepresentable {
         
-        private let parent: SBBTextArea
+        @Binding private var text: String
+        @Binding private var isEditing: Bool
+        private var label: String?
+        private let descriptionLabel = UILabel()
+        private let placeholderLabel = UILabel()
         
-        public init(_ parent: SBBTextArea) {
-            self.parent = parent
-        }
-    
-        public func textViewDidChange(_ textView: UITextView) {
-            parent.text = textView.text
-            updatePlaceholder(for: textView)
+        init(text: Binding<String>, isEditing: Binding<Bool>, label: String?) {
+            self._text = text
+            self._isEditing = isEditing
+            self.label = label
         }
         
-        public func updatePlaceholder(for textView: UITextView) {
-            let hasContent = !textView.text.isEmpty
-            parent.descriptionLabel.isHidden = !hasContent
-            parent.placeholderLabel.isHidden = hasContent
+        func makeUIView(context: Context) -> UITextView {
+            let view = UITextView()
+            view.delegate = context.coordinator
+            
+            let scaledFontSize = UIFontMetrics.default.scaledValue(for: 16)
+            view.font = UIFont(name: "SBBWeb-Light", size: scaledFontSize)
+            view.textContainerInset = UIEdgeInsets(top: 17, left: 11, bottom: 16, right: 11)
+            view.backgroundColor = UIColor.clear
+            let placeholderScaledFontSize = UIFontMetrics.default.scaledValue(for: 10)
+            view.textContainerInset = UIEdgeInsets(top: placeholderScaledFontSize + 7, left: 11, bottom: 16, right: 11)
+            
+            if let placeholder = label {
+                descriptionLabel.text = NSLocalizedString(placeholder, comment: "")
+                view.accessibilityLabel = placeholder
+                view.accessibilityValue = text
+            }
+            let descriptionScaledFontSize = UIFontMetrics.default.scaledValue(for: 10)
+            descriptionLabel.font = UIFont(name: "SBBWeb-Light", size: descriptionScaledFontSize)
+            descriptionLabel.textColor = UIColor(named: "placeholder", in: Helper.bundle, compatibleWith: nil)
+            view.addSubview(descriptionLabel)
+            
+            descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+            let descriptionTopConstraint = descriptionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 5)
+            let descriptionLeadingConstraint = descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 17)
+            let descriptionTrailingConstraint = descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            view.addConstraints([descriptionTopConstraint, descriptionLeadingConstraint, descriptionTrailingConstraint])
+            
+            if let placeholder = label {
+                placeholderLabel.text = NSLocalizedString(placeholder, comment: "")
+            }
+            placeholderLabel.font = UIFont(name: "SBBWeb-Light", size: scaledFontSize)
+            placeholderLabel.textColor = UIColor(named: "placeholder", in: Helper.bundle, compatibleWith: nil)
+            view.addSubview(placeholderLabel)
+            
+            placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+            let topConstraint = placeholderLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 17.5)
+            let leadingConstraint = placeholderLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+            let trailingConstraint = placeholderLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            view.addConstraints([topConstraint, leadingConstraint, trailingConstraint])
+            
+            let hasContent = !text.isEmpty
+            descriptionLabel.isHidden = !hasContent
+            placeholderLabel.isHidden = hasContent
+            
+            return view
+        }
+        
+        func updateUIView(_ uiView: UITextView, context: Context) {
+            uiView.text = text
+            context.coordinator.updatePlaceholder(for: uiView)
+            
+            if context.environment.isEnabled {
+                uiView.textColor = UIColor(named: "textBlack", in: Helper.bundle, compatibleWith: nil)
+            } else {
+                uiView.textColor = UIColor(named: "textMetal", in: Helper.bundle, compatibleWith: nil)
+            }
+        }
+        
+        func makeCoordinator() -> TextAreaCoordinator {
+            return TextAreaCoordinator(self)
+        }
+        
+        class TextAreaCoordinator: NSObject, UITextViewDelegate {
+            
+            private let parent: SBBTextAreaImpl
+            
+            init(_ parent: SBBTextAreaImpl) {
+                self.parent = parent
+            }
+            
+            func textViewDidChange(_ textView: UITextView) {
+                parent.text = textView.text
+                updatePlaceholder(for: textView)
+            }
+            
+            func updatePlaceholder(for textView: UITextView) {
+                let hasContent = !textView.text.isEmpty
+                parent.descriptionLabel.isHidden = !hasContent
+                parent.placeholderLabel.isHidden = hasContent
+            }
+            
+            func textViewDidBeginEditing(_ textView: UITextView) {
+                DispatchQueue.main.async {
+                    self.parent.isEditing = true
+                }
+            }
+            
+            func textViewDidEndEditing(_ textView: UITextView) {
+                DispatchQueue.main.async {
+                    self.parent.isEditing = false
+                }
+            }
         }
     }
 }
