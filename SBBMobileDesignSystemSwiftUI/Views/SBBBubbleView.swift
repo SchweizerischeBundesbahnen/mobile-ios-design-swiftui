a39c6efc95ad54611d6e66f885a33e6a3b1235e2
nicolasbrunner <nicolas.brunner@sbb.ch>
2020-08-14 14:12:53 +0200

    SBBBubbleView now also accepts non collapsible custom content.

--- SBBMobileDesignSystemSwiftUI/Views/SBBBubbleView.swift
+++ SBBMobileDesignSystemSwiftUI/Views/SBBBubbleView.swift
@@ -5,8 +5,22 @@
 import SwiftUI
 
 // initializer without detail
-public extension SBBBubbleView where Content == EmptyView {
-    init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, expanded: Binding<Bool>, extendNavigationBarBackground: Bool = true) {
+public extension SBBBubbleView where ExpandableContent == EmptyView, FixedContent == EmptyView {
+    init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, extendNavigationBarBackground: Bool = true) {
+        self.image = image
+        self.title = title
+        self.titleAccessibility = titleAccessibility
+        self.subtitle = subtitle
+        self.subtitleAccessibility = subtitleAccessibility
+        self._expanded = .constant(false)
+        self.extendNavigationBarBackground = extendNavigationBarBackground
+        self.expandableContent = nil
+        self.fixedContent = nil
+    }
+}
+
+public extension SBBBubbleView where FixedContent == EmptyView {
+    init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, expanded: Binding<Bool>, extendNavigationBarBackground: Bool = true, @ViewBuilder expandableContent: @escaping () -> ExpandableContent) {
         self.image = image
         self.title = title
         self.titleAccessibility = titleAccessibility
@@ -14,11 +28,26 @@ public extension SBBBubbleView where Content == EmptyView {
         self.subtitleAccessibility = subtitleAccessibility
         self._expanded = expanded
         self.extendNavigationBarBackground = extendNavigationBarBackground
-        self.content = nil
+        self.expandableContent = expandableContent()
+        self.fixedContent = nil
     }
 }
 
-public struct SBBBubbleView<Content>: View where Content: View {
+public extension SBBBubbleView where ExpandableContent == EmptyView {
+    init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, extendNavigationBarBackground: Bool = true, @ViewBuilder fixedContent: @escaping () -> FixedContent) {
+        self.image = image
+        self.title = title
+        self.titleAccessibility = titleAccessibility
+        self.subtitle = subtitle
+        self.subtitleAccessibility = subtitleAccessibility
+        self._expanded = .constant(false)
+        self.extendNavigationBarBackground = extendNavigationBarBackground
+        self.expandableContent = nil
+        self.fixedContent = fixedContent()
+    }
+}
+
+public struct SBBBubbleView<ExpandableContent, FixedContent>: View where ExpandableContent: View, FixedContent: View {
     
     private let image: Image
     private let title: Text
@@ -27,11 +56,12 @@ public struct SBBBubbleView<Content>: View where Content: View {
     private let subtitleAccessibility: Text?
     @Binding private var expanded: Bool
     private let extendNavigationBarBackground: Bool
-    private let content: Content?
+    private let expandableContent: ExpandableContent?
+    private let fixedContent: FixedContent?
     
     @Environment(\.sizeCategory) var sizeCategory
     
-    public init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, expanded: Binding<Bool>, extendNavigationBarBackground: Bool = true, @ViewBuilder content: @escaping () -> Content) {
+    public init(image: Image, title: Text, titleAccessibility: Text? = nil, subtitle: Text? = nil, subtitleAccessibility: Text? = nil, expanded: Binding<Bool>, extendNavigationBarBackground: Bool = true, @ViewBuilder expandableContent: @escaping () -> ExpandableContent, @ViewBuilder fixedContent: @escaping () -> FixedContent) {
         self.image = image
         self.title = title
         self.titleAccessibility = titleAccessibility
@@ -39,7 +69,8 @@ public struct SBBBubbleView<Content>: View where Content: View {
         self.subtitleAccessibility = subtitleAccessibility
         self._expanded = expanded
         self.extendNavigationBarBackground = extendNavigationBarBackground
-        self.content = content()
+        self.expandableContent = expandableContent()
+        self.fixedContent = fixedContent()
     }
     
     public var body: some View {
@@ -50,68 +81,74 @@ public struct SBBBubbleView<Content>: View where Content: View {
                     .frame(idealWidth: .infinity, minHeight: 35, maxHeight: 35)
             }
             HStack {
-                VStack(alignment: .leading, spacing: 8) {
-                    VStack(alignment: .leading, spacing: 0) {
-                        HStack(alignment: .center) {
-                            if !SizeCategories.accessibility.contains(sizeCategory) {
-                                image
-                                    .frame(width: 36, height: 36, alignment: .center)
-                                    .accessibility(hidden: true)
-                            }
-                            VStack(alignment: .leading, spacing: 8) {
-                                title
-                                    .sbbFont(.titleDefault)
-                                    .fixedSize(horizontal: false, vertical: true)
-                                    .accessibility(label: self.titleAccessibility ?? self.title)
-                                if (self.subtitle != nil) {
-                                    subtitle!
-                                        .sbbFont(.body)
+                VStack(alignment: .leading, spacing: 12) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        VStack(alignment: .leading, spacing: 0) {
+                            HStack(alignment: .center) {
+                                if !SizeCategories.accessibility.contains(sizeCategory) {
+                                    image
+                                        .frame(width: 36, height: 36, alignment: .center)
+                                        .accessibility(hidden: true)
+                                }
+                                VStack(alignment: .leading, spacing: 8) {
+                                    title
+                                        .sbbFont(.titleDefault)
                                         .fixedSize(horizontal: false, vertical: true)
-                                        .accessibility(label: self.subtitleAccessibility ?? self.subtitle!)
+                                        .accessibility(label: self.titleAccessibility ?? self.title)
+                                    if (self.subtitle != nil) {
+                                        subtitle!
+                                            .sbbFont(.body)
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .accessibility(label: self.subtitleAccessibility ?? self.subtitle!)
+                                    }
+                                }
+                                Spacer()
+                                if (expandableContent != nil) {
+                                    Group {
+                                        Image(sbbName: "chevron-small-up", size: .small)
+                                            .rotationEffect(.degrees(self.expanded ? 0 : 180))
+                                    }
+                                        .accessibility(hidden: true)
+                                        .frame(width: 32, height: 32)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(SBBColor.border))
                                 }
                             }
-                            Spacer()
-                            if (content != nil) {
-                                Group {
-                                    Image(sbbName: "chevron-small-up", size: .small)
-                                        .rotationEffect(.degrees(self.expanded ? 0 : 180))
+                        }
+                        if (expandableContent != nil) && self.expanded {
+                            HStack(spacing: 0) {
+                                if !SizeCategories.accessibility.contains(sizeCategory) {
+                                    Spacer(minLength: 44)
                                 }
-                                    .accessibility(hidden: true)
-                                    .frame(width: 32, height: 32)
-                                    .clipShape(Circle())
-                                    .overlay(Circle().stroke(SBBColor.border))
+                                VStack(alignment: .leading, spacing: 0) {
+                                    SBBDivider()
+                                    expandableContent
+                                        .sbbFont(.body)
+                                        .padding(.top, 8)
+                                }
                             }
                         }
                     }
-                    if (content != nil) && self.expanded {
-                        HStack(spacing: 0) {
-                            if !SizeCategories.accessibility.contains(sizeCategory) {
-                                Spacer(minLength: 44)
-                            }
-                            VStack(alignment: .leading, spacing: 0) {
-                                SBBDivider()
-                                content
-                                    .sbbFont(.body)
-                                    .padding(.top, 8)
-                            }
-                        }
+                        .accessibilityElement(children: .combine)
+                        .accessibility(hint: ((self.expandableContent == nil) ? Text("") : self.expanded ? Text("collapse".localized) : Text("expand".localized)))
+                        
+                    if fixedContent != nil {
+                        fixedContent
+                            .padding(.top, 4)
                     }
                 }
-                    .disabled(self.content == nil)
                     .padding(16)
-                    .accessibilityElement(children: .combine)
-                    .accessibility(hint: ((self.content == nil) ? Text("") : self.expanded ? Text("collapse".localized) : Text("expand".localized)))
                     .background(SBBColor.tabViewBackground)
                     .cornerRadius(16)
                     .shadow(color: Color.black.opacity(0.1), radius: 5)
                     .accentColor(SBBColor.textBlack)
+                    .onTapGesture {
+                        withAnimation{
+                            self.expanded.toggle()
+                        }
+                    }
             }
                 .padding(.horizontal, 16)
-                .onTapGesture {
-                    withAnimation{
-                        self.expanded.toggle()
-                    }
-                }
         }
     }
 }
@@ -119,40 +156,55 @@ public struct SBBBubbleView<Content>: View where Content: View {
 struct SBBBubbleView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true))
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"))
                 .previewDisplayName("Title only")
-            SBBBubbleView(image: Image(sbbName: "station", size: .medium), title: Text("Biel / Bienne"), subtitle: Text("Gleis 2 und 3."), expanded: .constant(true))
+            SBBBubbleView(image: Image(sbbName: "station", size: .medium), title: Text("Biel / Bienne"), subtitle: Text("Gleis 2 und 3."))
                 .previewDisplayName("Subtitle")
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true)) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
-            }
+            })
                 .previewDisplayName("Detail")
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), extendNavigationBarBackground: false) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), extendNavigationBarBackground: false, expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
-            }
+            })
                 .previewDisplayName("Detail, no NavigationBar background extension")
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true)) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
-            }
+            })
                 .previewDisplayName("Detail dark")
                 .environment(\.colorScheme, .dark)
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true)) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
-            }
+            })
                 .previewDisplayName("Accessibility Text Size (no icon)")
                 .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true)) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
                 Text("ca. +12'")
                     .foregroundColor(SBBColor.red)
                     .font(.sbbTitleDefault)
-            }
+            })
                 .previewDisplayName("Detail - multiple views")
-            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("R2 nach La Chaux-de-Fonds-Grenier, Armes-Réunies"), expanded: .constant(true)) {
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("R2 nach La Chaux-de-Fonds-Grenier, Armes-Réunies"), expanded: .constant(true), expandableContent: {
                 Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
-            }
+            })
                 .previewDisplayName("Long title")
-            
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), fixedContent: {
+                SBBSegmentedPicker(selection: .constant(0), tags: [0, 1], content: {
+                    Text("Wagen")
+                    Text("Perlschnur")
+                })
+            })
+                .previewDisplayName("Fixed Content")
+            SBBBubbleView(image: Image(sbbName: "train", size: .medium), title: Text("IC6 nach Basel"), expanded: .constant(true), expandableContent: {
+                Text("Wagen 3, 1. Klasse.\nBusiness-Zone, Ruhezone.\nNächster Halt: Olten um 17:03.")
+            }, fixedContent: {
+                SBBSegmentedPicker(selection: .constant(0), tags: [0, 1], content: {
+                    Text("Wagen")
+                    Text("Perlschnur")
+                })
+            })
+                .previewDisplayName("Expandable & Fixed Content")
         }
             .previewLayout(.sizeThatFits)
     }
