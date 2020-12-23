06a885935f51c3d6b1733c45f52486c95f7514b0
Terence Alberti <terence.alberti@sbb.ch>
2020-12-23 16:49:44 +0100

    create a uihostingcontroller with fixed size

--- SBBMobileDesignSystemSwiftUIDemoTests/SBBSnapshotTest.swift
+++ SBBMobileDesignSystemSwiftUIDemoTests/SBBSnapshotTest.swift
@@ -11,7 +11,7 @@ import SwiftUI
 extension SwiftUI.View {
     func toVC() -> UIViewController {
         let vc = UIHostingController(rootView: self)
-        vc.view.frame = UIScreen.main.bounds
+        vc.view.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
         return vc
     }
 }
