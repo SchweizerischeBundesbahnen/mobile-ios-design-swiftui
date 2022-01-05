d76205f288e5928fdc17b2ccfc1f1074dfac7bb1
nicolasbrunner <nici-b@hotmail.com>
2022-01-05 15:24:04 +0100

    Correct Swift Package Name in declaration file.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SBBMobileDesignSystemSwiftUI",
+    name: "SBBDesignSystemMobileSwiftUI",
     defaultLocalization: "en",
     platforms: [
         .iOS(.v14),
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SBBMobileDesignSystemSwiftUI",
-            targets: ["SBBMobileDesignSystemSwiftUI"]),
+            name: "SBBDesignSystemMobileSwiftUI",
+            targets: ["SBBDesignSystemMobileSwiftUI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +23,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SBBMobileDesignSystemSwiftUI",
+            name: "SBBDesignSystemMobileSwiftUI",
             dependencies: [],
-            path: "./SBBMobileDesignSystemSwiftUI",
+            path: "./SBBDesignSystemMobileSwiftUI",
             exclude: ["Info.plist", "SBBBundle.swift"],
             resources: [
                 .process("Fonts/Files")
