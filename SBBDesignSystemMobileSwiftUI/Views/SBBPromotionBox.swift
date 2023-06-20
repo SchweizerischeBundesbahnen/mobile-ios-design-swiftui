1ac3e10a642ca371be5d33a304af7cad93e411fb
Jeanne <25933880+fleuryj@users.noreply.github.com>
2023-06-20 18:18:52 +0200

    Add tests, demo, fix background SBBPromotionBox

--- SBBDesignSystemMobileSwiftUI/Views/SBBPromotionBox.swift
+++ SBBDesignSystemMobileSwiftUI/Views/SBBPromotionBox.swift
@@ -4,6 +4,23 @@
 
 import SwiftUI
 
+
+/**
+ A  View that is used to display a promotion box.
+ 
+ ## Overview
+ You create a SBBPromotionBox by providing a title and a text message, both as Text, and a binding for a boolean that decides wether the promotion box is displayed or not. Additionnally, one can use the parameter `newIn: String?` to provide for example the name of the app, the VoiceOver then reads: "New in DSM App. New feature. There is a new feature. Close this note. Button.".
+ 
+ ```swift
+ 
+ @State var isPresented: Bool = true
+ 
+ var body: some View {
+    SBBPromotionBox(newIn: "DSM App", title: Text("New feature"), text: Text("There is a new feature", isPresented: $isPresented)
+ }
+ ```
+ ![SBBPromotionBox](SBBPromotionBox)
+ */
 public struct SBBPromotionBox: View {
     
     @Environment(\.colorScheme) var colorScheme
@@ -16,6 +33,16 @@ public struct SBBPromotionBox: View {
     @Binding var isPresented: Bool
     @State var textSize: CGSize = .zero
     
+    
+    /**
+     Returns a SBBPromotionBox.
+     
+     - Parameters:
+        - newIn: A  optional string, for example the name of the app, to be read by VoiceOver.
+        - title: The Text to display as the title of the message.
+        - text: The Text to display as the title of the message.
+        - isPresented: Whether the promotion box is displayed.
+     */
     public init(newIn: String? = nil, title: Text, text: Text, isPresented: Binding<Bool>) {
         self.newIn = newIn
         self.title = title
@@ -66,19 +93,19 @@ public struct SBBPromotionBox: View {
                     .padding(16)
                     .background(
                         LinearGradient(gradient: Gradient(colors: colorScheme == .dark ? [
-                            Color(red: 46 / 255, green: 56 / 255, blue: 71 / 255),
-                            Color(red: 59 / 255, green: 69 / 255, blue: 87 / 255),
-                            Color(red: 59 / 255, green: 69 / 255, blue: 87 / 255),
-                            Color(red: 32 / 255, green: 41 / 255, blue: 54 / 255)] : [
-                                Color(red: 216 / 255, green: 236 / 255, blue: 237 / 255),
-                                Color(red: 232 / 255, green: 243 / 255, blue: 247 / 255),
-                                Color(red: 227 / 255, green: 239/255, blue: 243 / 255),
-                                Color(red: 216 / 255, green: 236 / 255, blue: 237 / 255)]), startPoint: .top, endPoint: .bottom)
+                            Color(red: 0.125, green: 0.161, blue: 0.212),
+                            Color(red: 0.18, green: 0.22, blue: 0.278),
+                            Color(red: 0.125, green: 0.161, blue: 0.212)] : [
+                                Color(red: 0.846, green: 0.926, blue: 0.93),
+                                Color(red: 0.91, green: 0.954, blue: 0.97),
+                                Color(red: 0.89, green: 0.937, blue: 0.953),
+                                Color(red: 0.84, green: 0.926, blue: 0.93)]), startPoint: .top, endPoint: .bottom)
                         .overlay(
                             Image("noise", bundle: Helper.bundle)
                                 .resizable(resizingMode: .tile)
                                 .aspectRatio(contentMode: .fill)
-                                .opacity(0.5))
+                                .compositingGroup()
+                                .blendMode(.overlay))
                     )
                     .cornerRadius(16)
                     .overlay(
