cb474e3b5c92d8d63723a7a9fb3cc6d9b87e870b
Jeanne <25933880+fleuryj@users.noreply.github.com>
2025-12-12 10:57:51 +0100

    try and fix xcode cloud metal error

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Get Xcode major version
+# `xcodebuild -version` returns something like:
+# Xcode 26.0.1
+# Build version 17A400
+XCODE_VERSION=$(xcodebuild -version | head -n 1 | awk '{print $2}' | cut -d. -f1)
+ 
+if [ "$XCODE_VERSION" -ge 26 ]; then
+    if xcodebuild -showComponent metalToolchain >/dev/null 2>&1; then
+        echo "Metal toolchain is installed"
+    else
+        echo "Metal toolchain is not installed. Failing the build."
+        exit 1
+    fi
+fi
