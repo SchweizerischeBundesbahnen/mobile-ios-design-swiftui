08f08d869a0f4b44de31cf2ca38af59eb78eaa39
Jeanne <25933880+fleuryj@users.noreply.github.com>
2026-02-24 17:10:56 +0100

    DSM min iOS is 15

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SBBDesignSystemMobileSwiftUI",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
